7c30b7c5fcc430db80a1514a227ccea509ded464
fadlmms <fadlmms@gmail.com>
2020-08-05 07:08:43 +0200

    adding form control

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,31 @@
+<template>
+      <input 
+        class="btn"
+       :type="type"
+       :value="value" 
+       >
+</template>
+
+<script>
+export default {
+    name:"base-button",
+    props: {
+        type: {
+            type:String,
+            required:true
+        },
+        value: {
+            type:String,
+            default:"submit"
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+    .btn {
+        width: 20px;
+        height: 20px;
+    }
+</style>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,76 @@
+ cvzbxncm0
+ .02<template>
+  <div class="field">
+    <label class="field_label">{{label}}</label>
+    <input 
+        class="field_input"
+        :type="type"
+        :id="id"
+        :placeholder="placeholder"
+        :value="value"
+        :required="required"
+        v-on:input="updateValue($event.target.value)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+    name:"base-input",
+    props: {
+        id: {
+            type:String
+        },
+        type: {
+            type:String,
+            default: "text"    
+        },
+        label: {
+            type:String,
+            required: true
+        },
+        value: {
+            type:String
+        },
+        placeholder: {
+            type:String
+        },
+        required: {
+            type:Boolean,
+            default: false
+        }
+    },
+    methods: {
+        updateValue: function(value){
+            this.$emit('input',value);
+        }
+    }
+   
+}
+</script>
+
+<style scoped>
+/*
+    BEM 
+    field
+    field__input 
+    field__input--color 
+    field__input--bg
+
+*/
+.field {
+    height: 30px;
+    border: 1px solid #e2e2e2;
+    border-radius: 2px;
+    margin-bottom: 3%;
+}
+.field_input {
+    width: 100%;
+    height: 100%;
+    padding-top: 2%;
+}
+.field_label {
+    position: absolute;
+    padding: 0.4%;
+}
+</style>
